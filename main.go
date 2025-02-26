d67b31b4a68c897c8f520878249e89e888a93f74
ZUlykpanova <zulykpanova@beeline.kz>
2025-02-26 15:00:40 +0500

    commands

--- main.go
+++ main.go
@@ -5,10 +5,27 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
+	supportedCommands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Show this help message",
+			callback:    commandHelp,
+		},
+	}
 	scanner := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
@@ -17,7 +34,25 @@ func main() {
 			log.Fatal(err)
 		}
 		text := scanner.Text()
-		cleanText := strings.Fields(strings.ToLower(text))
-		fmt.Printf("Your command was: %s\n", cleanText[0])
+		value, ok := supportedCommands[text]
+		if ok {
+			err := value.callback()
+			if err != nil {
+				fmt.Print(err.Error())
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
+}
+
+func commandExit() error {
+	fmt.Print("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Print("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex\n")
+	return nil
 }
