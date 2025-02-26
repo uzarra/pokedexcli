e04abf1c290eccb95b2489a85b1b50762b5b2ba2
ZUlykpanova <zulykpanova@beeline.kz>
2025-02-26 14:45:28 +0500

    repl

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 	"strings"
 )
 
-func cleanInput(s string) []string {
-	return strings.Fields(s)
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+		text := scanner.Text()
+		cleanText := strings.Fields(strings.ToLower(text))
+		fmt.Printf("Your command was: %s\n", cleanText[0])
+	}
 }
